1fb50f539b5f61e47bad9b31f704049951d649ab
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-08-04 10:47:25 -0300

    feat: add index

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Workflow } = require("./src/facade/workflow");
+const { Process } = require("./src/facade/process");
+const { ProcessState } = require("./src/facade/processState");
+const { Coverage } = require("./src/facade/coverage");
+
+module.exports = {
+  Workflow,
+  Process,
+  ProcessState,
+  Coverage,
+};
