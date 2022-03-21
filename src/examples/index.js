e94e7eb05ea8de25d7d275b547f0af0ebac4bfc9
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-03-21 14:19:21 -0300

    refactor: search workflow by blueprint name

--- src/examples/index.js
+++ src/examples/index.js
@@ -5,10 +5,13 @@ const { Coverage } = require("../facade/coverage");
 const db = Knex(knexConfig);
 
 const doit = async () => {
-  const workflowId = "f81cf040-6d41-11eb-82af-1d1728424c5c";
+  const workflow = await db('workflow')
+        .where('name', 'basic')
+        .orderBy('created_at', 'desc')
+        .first();
 
   const coverage = new Coverage(db);
-  const result = await coverage.calculateCoverage(workflowId, 50);
+  const result = await coverage.calculateCoverage(workflow.id, 50);
 
   console.log(result);
   console.log("uncovered Nodes: ", result.coverage.nodes.uncovered);
