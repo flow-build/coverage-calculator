97c4392976f92e3f8e7901714d8931b66caeab87
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-05-10 12:53:15 -0300

    feat: expose coverage script on index.js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const { Workflow } = require("./src/facade/workflow");
-const { Process } = require("./src/facade/process");
-const { ProcessState } = require("./src/facade/processState");
+require("dotenv").config();
+const Knex = require("knex");
+const knexConfig = require("./knexfile")["development"];
 const { Coverage } = require("./src/facade/coverage");
+const db = Knex(knexConfig);
+const fs = require('fs');
 
-module.exports = {
-  Workflow,
-  Process,
-  ProcessState,
-  Coverage,
-};
+class BlueprintCoverage {
+  constructor() {
+    this.coverage = new Coverage(db);
+  }
+
+  async analyze(workflowName, count, testsResult) {
+
+    const workflow = await db('workflow')
+      .where('name', workflowName)
+      .orderBy('created_at', 'desc')
+      .first();
+
+    const result = await this.coverage.calculateCoverage(workflow.id, count);
+    result.processes.testsResult = testsResult?.map(obj => obj.status).reverse();
+    const now = new Date();
+    const reportFileName = (workflowName + now.getFullYear() + (now.getMonth() + 1) + now.getDate() + now.getHours() + now.getMinutes() + now.getSeconds());
+    
+    if (!fs.existsSync("tests")) {
+      fs.mkdirSync("tests");
+    }
+    if (!fs.existsSync("tests/coverageReports")) {
+      fs.mkdirSync("tests/coverageReports");
+    }
+    fs.writeFileSync(`tests/coverageReports/${reportFileName}.json`, JSON.stringify(result), err => {
+      if (err) throw err;
+    });
+
+    setTimeout(function () {
+      process.exit();
+    }, 5000);
+  }
+}
+
+module.exports = { BlueprintCoverage };
