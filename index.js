412410f665d9f8d62072cfb0236c86820486e8f2
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-06-06 18:57:04 -0300

    feat: add report script on index export

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const steps = require("./tests/features/support/steps");
 const world = require("./tests/features/support/world");
 const hooks = require("./tests/features/support/hooks");
+const report = require("./scripts/report")
 const { BlueprintCoverage } = require("./src/utils/blueprintCoverage");
 
 module.exports = { 
   BlueprintCoverage,
   steps,
   hooks,
-  world
+  world,
+  report
 };
