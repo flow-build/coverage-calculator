39cfbcc3b312d1d10fb29e3676d0bfcdbc402700
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-06-06 12:53:34 -0300

    fix: adjust index with new coverage result format

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { Coverage } = require("./src/facade/coverage");
 const db = Knex(knexConfig);
 const fs = require('fs');
 
+if (!fs.existsSync("tests/features/support/coverageReports")) {
+  fs.mkdirSync("tests/features/support/coverageReports");
+}
+
 class BlueprintCoverage {
   constructor() {
     this.coverage = new Coverage(db);
@@ -18,24 +22,21 @@ class BlueprintCoverage {
       .first();
 
     const result = await this.coverage.calculateCoverage(workflow.id, count);
-    result.processes.testsResult = testsResult?.map(obj => obj.status).reverse();
+    result.processes.testsResult = testsResult.map(obj => obj.status).reverse();
     const now = new Date();
     const reportFileName = (workflowName + now.getFullYear() + (now.getMonth() + 1) + now.getDate() + now.getHours() + now.getMinutes() + now.getSeconds());
     
-    if (!fs.existsSync("tests")) {
-      fs.mkdirSync("tests");
-    }
-    if (!fs.existsSync("tests/coverageReports")) {
-      fs.mkdirSync("tests/coverageReports");
-    }
-    fs.writeFileSync(`tests/coverageReports/${reportFileName}.json`, JSON.stringify(result), err => {
+    fs.writeFileSync(`tests/features/support/coverageReports/${reportFileName}.json`, JSON.stringify(result), err => {
       if (err) throw err;
     });
 
     setTimeout(function () {
       process.exit();
     }, 5000);
+
+
   }
+
 }
 
 module.exports = { BlueprintCoverage };
