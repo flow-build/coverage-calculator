4fe1a49b60ba98ebee4fa014ef3e42bce45ff427
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-06-06 16:12:42 -0300

    refactor: let index.js just export methods

--- index.js
+++ index.js
@@ -1,48 +1,11 @@
-require("dotenv").config();
-const Knex = require("knex");
-const knexConfig = require("./knexfile")["development"];
-const { Coverage } = require("./src/facade/coverage");
-const steps = require("./tests/features/support/steps")
-const report = require("./scripts/report")
-const db = Knex(knexConfig);
-const fs = require("fs");
-
-if (!fs.existsSync("tests/features/support/coverageReports")) {
-  fs.mkdirSync("tests/features/support/coverageReports");
-}
-
-class BlueprintCoverage {
-  constructor() {
-    this.coverage = new Coverage(db);
-  }
-
-  async analyze(workflowName, count, testsResult) {
-
-    const workflow = await db('workflow')
-      .where('name', workflowName)
-      .orderBy('created_at', 'desc')
-      .first();
-
-    const result = await this.coverage.calculateCoverage(workflow.id, count);
-    result.processes.testsResult = testsResult.map(obj => obj.status).reverse();
-    const now = new Date();
-    const reportFileName = (workflowName + now.getFullYear() + (now.getMonth() + 1) + now.getDate() + now.getHours() + now.getMinutes() + now.getSeconds());
-    
-    fs.writeFileSync(`tests/features/support/coverageReports/${reportFileName}.json`, JSON.stringify(result), err => {
-      if (err) throw err;
-    });
-
-    setTimeout(function () {
-      process.exit();
-    }, 5000);
-
-
-  }
-
-}
+const steps = require("./tests/features/support/steps");
+const world = require("./tests/features/support/world");
+const hooks = require("./tests/features/support/hooks");
+const { BlueprintCoverage } = require("./src/utils/blueprintCoverage");
 
 module.exports = { 
   BlueprintCoverage,
   steps,
-  report
+  hooks,
+  world
 };
